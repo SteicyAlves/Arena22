b46fcdbaea8d77c9a8a0a389a191bc727747e32c
Steicy Alves <steicy.alves@arena22.com.br>
2022-03-04 19:29:59 -0300

    Removendo sobrenome da model de usuário e factory

--- arena22-web/tests/features/support/libs/factories/user_factory.rb
+++ arena22-web/tests/features/support/libs/factories/user_factory.rb
@@ -3,14 +3,13 @@ require_relative '../../services/user_service'
 
 FactoryBot.define do
     factory :user_with_optional, class: UserModel do
-        name { 'Automated' }
-        last_name { 'User Optional' } 
+        email { 'testeemail.arena22@gmail.com' } 
+        password { 'Aren@12345678%' } 
         nickname { 'Optional Tested' }
+        name { 'Automated User Optional' }
         cpf { Faker::IDNumber.brazilian_citizen_number(formatted: true) } 
         birth_date { '12/10/1995' } 
         phone_number { '69933333333' }
-        email { 'testeemail.arena22@gmail.com' } 
-        password { 'Aren@12345678%' } 
         coupon { 'SUCESSOCASH' }
 
         after(:build) do |user|
@@ -19,13 +18,13 @@ FactoryBot.define do
     end
 
     factory :user_only_requireds, class: UserModel do
-        name { 'Automated' }
-        last_name { 'User Requireds' }
-        nickname { 'Required Tested' }
-        birth_date { '12/10/1995' } 
-        phone_number { '69944444444' }
         email { 'testeemail.arena22@gmail.com' }
         password { 'Aren@12345678%' }
+        nickname { 'Required Tested' }
+        name { 'Automated User Requireds' }
+        cpf { Faker::IDNumber.brazilian_citizen_number(formatted: true) } 
+        birth_date { '02/08/1992' } 
+        phone_number { '69944444444' }
         
         after(:build) do |user|
             UserService.new.disable_user_by_email(user.email)
@@ -37,14 +36,13 @@ FactoryBot.define do
             external_account_data { nil }
         end
 
-        name { 'Automated' }
-        last_name { "User #{external_account_data[:type]}" }
+        email { external_account_data[:email] } 
+        password { external_account_data[:password] }
         nickname { external_account_data[:nickname] }
+        name { "Automated User #{external_account_data[:type]}" }
         cpf { Faker::IDNumber.brazilian_citizen_number(formatted: true) } 
         birth_date { '12/10/1995' } 
         phone_number { '68922222222' }
-        email { external_account_data[:email] } 
-        password { external_account_data[:password] }
         coupon { '#17248' }
 
         after(:build) do |user|
@@ -53,14 +51,13 @@ FactoryBot.define do
     end
 
     factory :random_user, class: UserModel do
-        name { Faker::Name.first_name }
-        last_name { Faker::Name.last_name } 
+        email { Faker::Internet.email(name: "#{name}_#{SecureRandom.alphanumeric(8)}", domain: 'testexample') }
+        password { 'Arena12345678' } 
         nickname { Faker::Games::Pokemon.name }
+        name { Faker::Name.name }
         cpf { Faker::IDNumber.brazilian_citizen_number(formatted: true) } 
         birth_date { Faker::Date.birthday(min_age: 18, max_age: 80).strftime('%d/%m/%Y') } 
         phone_number { Faker::PhoneNumber.cell_phone }
-        email { Faker::Internet.email(name: "#{name}_#{SecureRandom.alphanumeric(8)}", domain: 'testexample') }
-        password { 'Arena12345678' } 
         coupon { 'SUCESSOCASH' }
     end
 
@@ -69,14 +66,13 @@ FactoryBot.define do
             user_data { nil }
         end
 
-        name { user_data[:name] }
-        last_name { user_data[:last_name] } 
+        email { user_data[:email] } 
+        password { user_data[:password] }
         nickname { user_data[:nickname] }
+        name { user_data[:name] }       
         cpf { user_data[:cpf] } 
         birth_date { user_data[:birth_date] } 
         phone_number { user_data[:phone_number] }
-        email { user_data[:email] } 
-        password { user_data[:password] } 
         coupon { user_data[:coupon] }
     end
 
